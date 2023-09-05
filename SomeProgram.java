36320e542ab07e5340d1dfbf7b4244732f412822
Mikolaj <test@test.com>
2023-09-05 13:05:41 +0100

    kolejny commit

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,10 @@
 public class SomeProgram {
+    public void manageTheSatellite() {
+        // TODO implement
+    }
     public static void main(String[] args) {
         System.out.println("We are learning to use Git.");
+	    System.out.println("We will be Git experts, soon.");
+
     }
 }
